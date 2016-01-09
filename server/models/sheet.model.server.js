8b34bb48beb46c8cdc752d9ee07c2aa713399100
Mike DiPardo <mgdipardo@gmail.com>
2016-01-09 12:43:38 -0700

    Original

---
+++ server/models/sheet.model.server.js
@@ -0,0 +1,131 @@
+var q = require("q");
+
+module.exports = function(db, mongoose) {
+    var SheetSchema = require("./sheet.schema.server.js")(mongoose);
+    var SheetModel  = mongoose.model("SheetModel", SheetSchema);
+    var api = {
+        createSheet: createSheet,
+        readAllSheet: readAllSheet,
+        readOneSheet: readOneSheet,
+        updateSheet: updateSheet,
+        removeSheet: removeSheet,
+        createCell: createCell,
+        removeCell: removeCell,
+        updateCell: updateCell
+    };
+    return api;
+    // parameters= which sheet, which cell and the update
+    function updateCell(sheetId, cellIndex, cell) {
+        var deferred = q.defer();
+        // cells do not have its own schema
+        SheetModel.findById(sheetId, function(err, sheet){
+            // only updating the literal of the cell at this point
+            sheet.cells[cellIndex].literal = cell.literal;
+            sheet.save(function(err, sheet){
+                deferred.resolve(sheet);
+            });
+        });
+
+        return deferred.promise;
+    }
+    // parameters= which sheet and what the new cell is
+    function createCell(sheetId, cell) {
+        var deferred = q.defer();
+
+        SheetModel.findById(sheetId, function(err, sheet){
+            // again, the cell only has a literal at this point
+            sheet.cells.push(cell);
+            sheet.save(function(err, sheet){
+                deferred.resolve(sheet);
+            });
+        });
+
+        return deferred.promise;
+    }
+    // parameters= which sheet, and which cell
+    function removeCell(sheetId, cellIndex) {
+        var deferred = q.defer();
+        // splice removes the cell from the array
+        SheetModel.findById(sheetId, function(err, sheet){
+            sheet.cells.splice(cellIndex, 1);
+            sheet.save(function(err, sheet){
+                deferred.resolve(sheet);
+            });
+        });
+
+        return deferred.promise;
+    }
+
+    function createSheet(sheet) {
+        var deferred = q.defer();
+
+        SheetModel.create(sheet, function(err, sheet) {
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(sheet);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    function readAllSheet() {
+        var deferred = q.defer();
+
+        SheetModel.find(function(err, sheets){
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(sheets);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    function readOneSheet(id) {
+        var deferred = q.defer();
+
+        SheetModel.findById(id, function(err, sheet){
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(sheet);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    function updateSheet(id, sheet) {
+        var deferred = q.defer();
+        // If there is an _id property, remone it. This is done so
+        // the _id is not updated.
+        sheet.delete("_id");
+        // "where" _id=:id, "set sheet"
+        SheetModel.update({_id: id}, {$set: sheet}, function(err, sheet) {
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(sheet);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    function removeSheet(id) {
+        var deferred = q.defer();
+
+        SheetModel.remove({_id: id}, function(err, status) {
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(status);
+            }
+        });
+
+        return deferred.promise;
+    }
+}
---
+++ server/services/cell.service.server.js
@@ -0,0 +1,31 @@
+module.exports = function(app, model) {
+    app.post("/experiments/mongodb/ss/sheet/:sheetId/cell", createCell);
+    app.put("/experiments/mongodb/ss/sheet/:sheetId/cell/:cellIndex", updateCell);
+    app.delete("/experiments/mongodb/ss/sheet/:sheetId/cell/:cellIndex", removeCell);
+
+    // All these are extracting information from the http request and
+    // putting them in the model
+    function updateCell(req, res) {
+        model
+            .updateCell(req.params.sheetId, req.params.cellIndex, req.body)
+            .then(function(sheet) {
+                res.json(sheet);
+            });
+    }
+
+    function createCell(req, res) {
+        model
+            .createCell(req.params.sheetId, req.body)
+            .then(function(sheet){
+                res.json(sheet);
+            });
+    }
+
+    function removeCell(req, res) {
+        model
+            .removeCell(req.params.sheetId, req.params.cellIndex)
+            .then(function(sheet){
+                res.json(sheet);
+            });
+    }
+};
